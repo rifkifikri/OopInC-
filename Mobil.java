0f804caf69f914da614ae7e78cdd1edfde4bb842
rifkifikri <rifkifikri000@gmail.com>
2021-06-16 12:53:36 +0700

    oopJava

---
+++ Mobil.java
@@ -0,0 +1,32 @@
+
+public class Mobil {
+	//class parent
+	
+	int harga;
+	
+	String name="Toyota";//default
+
+	void namaToko() {//default atau tanpa modifier
+	
+		System.out.println("SRIKANDI MTR");
+	}
+	public void mobil() {
+		System.out.println("Mobil 1");
+	}
+	
+	public void setharga(int harga){
+		this.harga=harga;
+	}
+	
+	public int getharga() {
+		return harga;
+	}
+
+	public void jenis(){
+		System.out.println(name+" Pajero");
+	}
+	protected void beban() {
+		System.out.println("360 kg");
+	}
+	
+}
---
+++ Toyota.java
@@ -0,0 +1,31 @@
+
+public class Toyota extends Mobil{//subclass dari Mobil
+	public void mobil() {
+		System.out.println("Mobil 2");
+	}
+	public void jenis(){
+		System.out.println(name+" Alpart");
+				
+	}
+	protected void beban() {
+		System.out.println("800 kg");
+	}
+	public static void main(String[] args) {
+		Mobil produk= new Mobil();
+		produk.namaToko();
+		produk.mobil();
+		produk.jenis();
+		produk.setharga(6000000);
+		System.out.println("Rp "+produk.getharga());
+		produk.beban();
+		
+		Toyota merk= new Toyota();
+		merk.namaToko();
+		merk.mobil();
+		merk.jenis();
+		merk.setharga(30000000);
+		System.out.println("Rp "+merk.getharga());
+		merk.beban();
+	}
+	
+}
